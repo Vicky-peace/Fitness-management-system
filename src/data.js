3561e57be05a3ba6b0eb01bf30f4d4eebbeec4aa
Vicky-peace <victorpeace47@gmail.com>
2023-06-10 08:41:35 +0300

    values section added

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-import { SiOpenaigym} from 'react-icons/si'
+
 
 
 
@@ -44,30 +44,57 @@ export const links = [
 export const programs = [
   {
     id: 1,
-    icon: SiOpenaigym,
     title: "Strength and Conditioning",
     info: "Take your fitness to the next level with our comprehensive strength and conditioning program. Build muscle, increase endurance, and improve overall performance.",
     path: '/programs/111',
   },
   {
     id: 2,
-    icon: SiOpenaigym,
     title: "Cardio Blast",
     info:  "Get your heart pumping and burn calories with our high-energy cardio program. Improve cardiovascular health, boost metabolism, and achieve your weight loss goals.",
     path: '/programs/222',
   },
   {
     id: 3,
-    icon: SiOpenaigym,
     title: "Functional Fitness",
     info: "Enhance your everyday movement patterns and functional abilities with our functional fitness program. Improve flexibility, balance, and coordination for better performance in daily activities.",
     path: '/programs/333',
   },
   {
     id: 4,
-    icon: SiOpenaigym,
-    title: 'Program Four',
+    title: "Mind-Body Fusion",
     info: "Find balance and inner peace with our mind-body fusion program. Experience a combination of yoga, meditation, and mindful exercises to reduce stress and improve overall well-being.",
     path: '/programs/444',
   },
 ];
+
+
+
+export const values = [
+  {
+    id: 1,
+  
+    title: "Fitness Training",
+    desc: "Achieve your fitness goals through tailored workout programs and expert guidance."
+  },
+  {
+    id: 2,
+    
+    title: "Strength Building",
+    desc: "Build strength and increase muscle mass with effective weightlifting and resistance training."
+  },
+  {
+    id: 3,
+   
+    title: "Cardiovascular Fitness",
+    desc: "Improve your cardiovascular health and endurance with various cardio exercises and routines."
+  },
+  {
+    id: 4,
+  
+    title: "Group Classes",
+    desc: "Join fun and energetic group classes led by experienced instructors for a motivating workout."
+  }
+];
+
+export default values;
