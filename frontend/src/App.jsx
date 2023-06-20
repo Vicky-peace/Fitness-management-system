58b4abb62ccee7d58ce2154a997d59dddfa0c255
Vicky-peace <victorpeace47@gmail.com>
2023-06-20 12:41:55 +0300

    New Features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import SignUp from './pages/signup/SignUp'
 /*Components*/
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 import './App.css'
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
   return (
     <>
     <BrowserRouter>
+    <ScrollToTop />
     <Navbar/>
      <Routes>
      <Route index element={<Home/>}/>
--- frontend/src/pages/gallery/Gallery.jsx
+++ frontend/src/pages/gallery/Gallery.jsx
@@ -1,19 +1,48 @@
-import React from 'react'
-import Header from '../../components/Header'
-import HeaderImage from '../../images/header_bg_3.jpg'
-import './gallery.css'
+import React, { useEffect, useState } from 'react';
+import Header from '../../components/Header';
+import HeaderImage from '../../images/header_bg_3.jpg';
+import './gallery.css';
 
 const Gallery = () => {
-  
+  const galleryLength = 15;
+  const [images, setImages] = useState([]);
 
+  useEffect(() => {
+    const fetchImages = async () => {
+      const imagePromises = [];
+      for (let i = 1; i <= galleryLength; i++) {
+        imagePromises.push(import(`../../images/gallery${i}.jpg`));
+      }
+
+      try {
+        const resolvedImages = await Promise.all(imagePromises);
+        const imageUrls = resolvedImages.map((image) => image.default);
+        setImages(imageUrls);
+      } catch (error) {
+        console.error('Failed to load gallery images:', error);
+      }
+    };
+
+    fetchImages();
+  }, [galleryLength]);
 
   return (
     <>
-      <Header title="Our Gallery" image ={HeaderImage}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nisi vitae quasi fuga eaque est non, veniam velit tempore magni.
-      </Header>
-    </>
-  )
-}
+     <Header title="Our Gallery" image={HeaderImage}>
+  Discover the Beauty of Our Captivating Gallery Collection
+</Header>
 
-export default Gallery
+      <section className="gallery">
+        <div className="container gallery__container">
+          {images.map((image, index) => (
+            <article key={index}>
+              <img src={image} alt={`Gallery ${index + 1}`} />
+            </article>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Gallery;
